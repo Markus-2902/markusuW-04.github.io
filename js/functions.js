8808d9ad8b42ec3b33bac69b58467f4906ed1650
Markus Ullrich <ullrich.m04@htlwienwest.at>
2021-02-10 21:57:14 +0100

    original

--- js/functions.js
+++ js/functions.js
@@ -24,7 +24,9 @@ for (let index = 0; index < großbuchstaben.length; index++) {
 
 //wieviele buttons:
 function addemailbuttons(){
-
+    const div = document.getElementById("emailnamebuttons");
+    const button = document.createElement('button');
+    button.className = "floated2";
 }
 
 function addtldbuttons(){
